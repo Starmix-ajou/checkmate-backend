795d1d1182e6e2b2e2e2f401b62d965302470132
yeonnnnjs <sungyeon52@gmail.com>
2025-04-02 22:58:41 +0900

    fix: config 디렉토리 이름 변경 (CM-18)

--- src/main/java/com/starmix/checkmate/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/starmix/checkmate/infrastructure/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.starmix.checkmate.infrastructure.cofig;
+package com.starmix.checkmate.infrastructure.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
