139b094f63733f92c42850b7b2b203e0b7198ff7
yeonnnnjs <sungyeon52@gmail.com>
2025-04-02 22:58:41 +0900

    fix: config 디렉토리 이름 변경 (CM-18)

--- src/main/java/com/starmix/checkmate/infrastructure/config/WebClientConfig.java
+++ src/main/java/com/starmix/checkmate/infrastructure/config/WebClientConfig.java
@@ -1,4 +1,4 @@
-package com.starmix.checkmate.infrastructure.cofig;
+package com.starmix.checkmate.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
