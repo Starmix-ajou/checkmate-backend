f3437af528c834939973c3ab509dd2358e0c0051
yeonnnnjs <sungyeon52@gmail.com>
2025-04-17 22:15:22 +0900

    chore: Swagger Server 구성 (CM-48)

--- src/main/java/com/starmix/checkmate/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/starmix/checkmate/infrastructure/config/SwaggerConfig.java
@@ -1,30 +1,51 @@
 package com.starmix.checkmate.infrastructure.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Checkmate API")
+                .version("1.0.0")
+                .description("Checkmate API Docs");
+
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+
+        List<Server> servers = List.of(
+                new Server().url("https://api.checkmate.it.kr").description("Prod Server"),
+                new Server().url("http://localhost:8080").description("Local Server")
+        );
+
+        SecurityRequirement addSecurityItem = new SecurityRequirement();
+        addSecurityItem.addList("JWT");
+
+        Components components = new Components();
+        components.addSecuritySchemes("JWT", bearerAuth);
+
+
         return new OpenAPI()
-                .info(new Info()
-                        .title("Checkmate API")
-                        .description("Checkmate API Docs")
-                        .version("1.0.0"))
-                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("BearerAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                        )
-                );
+                .servers(servers)
+                .components(components)
+                .addSecurityItem(addSecurityItem)
+                .info(info);
     }
 }
+
