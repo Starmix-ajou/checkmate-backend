02be91b5552ca4592954ec42f75fb4ce902ac472
yeonnnnjs <sungyeon52@gmail.com>
2025-04-17 20:57:27 +0900

    feat: CustomException 추가 (CM-48)

--- src/main/java/com/starmix/checkmate/adapter/out/oauth/adapter/GoogleOAuthAdapter.java
+++ src/main/java/com/starmix/checkmate/adapter/out/oauth/adapter/GoogleOAuthAdapter.java
@@ -4,6 +4,8 @@ import com.starmix.checkmate.adapter.out.oauth.client.GoogleOAuthFeignClient;
 import com.starmix.checkmate.adapter.out.oauth.client.response.GoogleOAuthUserInfoResponse;
 import com.starmix.checkmate.adapter.out.oauth.dto.OAuthUserInfo;
 import com.starmix.checkmate.application.port.out.oauth.GoogleOAuthPort;
+import com.starmix.checkmate.global.exception.CustomException;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
@@ -16,10 +18,14 @@ public class GoogleOAuthAdapter implements GoogleOAuthPort {
 
     @Override
     public OAuthUserInfo getUserInfo(Jwt jwt) {
-        GoogleOAuthUserInfoResponse response = googleOAuthFeignClient.getUserInfo("Bearer " + jwt.getTokenValue());
-        return OAuthUserInfo.builder()
-                .email(response.getEmail())
-                .name(response.getName())
-                .build();
+        try {
+            GoogleOAuthUserInfoResponse response = googleOAuthFeignClient.getUserInfo("Bearer " + jwt.getTokenValue());
+            return OAuthUserInfo.builder()
+                    .email(response.getEmail())
+                    .name(response.getName())
+                    .build();
+        } catch (FeignException e) {
+            throw new CustomException("유효하지 않은 토큰입니다.");
+        }
     }
 }
