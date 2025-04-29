e3659dfc2ea5ba3f43c8bb165fe32de896561add
yeonnnnjs <sungyeon52@gmail.com>
2025-04-29 22:15:55 +0900

    feat: SSE keep-alive 추가 (CM-74)

--- src/main/java/com/starmix/checkmate/adapter/in/sse/common/SseEmitterManager.java
+++ src/main/java/com/starmix/checkmate/adapter/in/sse/common/SseEmitterManager.java
@@ -5,10 +5,18 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class SseEmitterManager {
     private final CopyOnWriteArrayList<SseEmitter> emitters = new CopyOnWriteArrayList<>();
+
+    public SseEmitterManager() {
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleAtFixedRate(this::sendKeepAlive, 30, 30, TimeUnit.SECONDS);
+    }
 
     public SseEmitter addEmitter() {
         SseEmitter emitter = new SseEmitter(0L);
@@ -24,6 +32,16 @@ public class SseEmitterManager {
         for (SseEmitter emitter : emitters) {
             try {
                 emitter.send(SseEmitter.event().name(eventName).data(data));
+            } catch (IOException e) {
+                emitters.remove(emitter);
+            }
+        }
+    }
+
+    private void sendKeepAlive() {
+        for (SseEmitter emitter : emitters) {
+            try {
+                emitter.send(SseEmitter.event().comment("keep-alive"));
             } catch (IOException e) {
                 emitters.remove(emitter);
             }
