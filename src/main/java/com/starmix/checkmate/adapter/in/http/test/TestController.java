41a70e7ae387256f964e51628916ebe8bfaa832f
yeonnnnjs <sungyeon52@gmail.com>
2025-04-29 21:50:32 +0900

    feat: 프로젝트 생성 Test API 추가 (CM-74)

--- src/main/java/com/starmix/checkmate/adapter/in/http/test/TestController.java
+++ src/main/java/com/starmix/checkmate/adapter/in/http/test/TestController.java
@@ -1,8 +1,11 @@
 package com.starmix.checkmate.adapter.in.http.test;
 
+import com.starmix.checkmate.adapter.in.sse.common.SseEmitterManager;
 import com.starmix.checkmate.adapter.in.sse.project.request.CreateFeatureDefinitionRequest;
+import com.starmix.checkmate.adapter.in.sse.project.request.FeedbackRequest;
 import com.starmix.checkmate.application.port.out.persistence.ProjectPersistencePort;
 import com.starmix.checkmate.application.port.out.persistence.UserPersistencePort;
+import com.starmix.checkmate.application.service.ProjectTestService;
 import com.starmix.checkmate.domain.project.Project;
 import com.starmix.checkmate.domain.user.User;
 import com.starmix.checkmate.global.exception.CustomException;
@@ -23,6 +26,33 @@ public class TestController {
     private final ProjectPersistencePort projectPersistencePort;
     private final UserPersistencePort userPersistencePort;
     private final JwtUtil jwtUtil;
+
+    private final ProjectTestService projectService;
+    private final SseEmitterManager sseEmitterManager;
+
+    @PostMapping("/definition")
+    public void createFeatureDefinition(@RequestBody CreateFeatureDefinitionRequest request) {
+        var response = projectService.createFeatureDefinition(request);
+        sseEmitterManager.sendEvent("create-feature-definition", response);
+    }
+
+    @PutMapping("/definition")
+    public void feedbackFeatureDefinition(@RequestBody FeedbackRequest request) {
+        var response = projectService.feedbackFeatureDefinition(request);
+        sseEmitterManager.sendEvent("feedback-feature-definition", response);
+    }
+
+    @GetMapping("/specification")
+    public void createFeatureSpecification() {
+        var response = projectService.createFeatureSpecification();
+        sseEmitterManager.sendEvent("create-feature-specification", response);
+    }
+
+    @PutMapping("/specification")
+    public void feedbackFeatureSpecification(@RequestBody FeedbackRequest request) {
+        var response = projectService.feedbackFeatureSpecification(request);
+        sseEmitterManager.sendEvent("feedback-feature-specification", response);
+    }
 
     @Transactional
     @PostMapping("/project/create")
--- src/main/java/com/starmix/checkmate/adapter/in/sse/project/ProjectSseController.java
+++ src/main/java/com/starmix/checkmate/adapter/in/sse/project/ProjectSseController.java
@@ -7,8 +7,6 @@ import com.starmix.checkmate.application.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/project")
@@ -18,25 +16,25 @@ public class ProjectSseController {
     private final SseEmitterManager sseEmitterManager;
 
     @PostMapping("/definition")
-    public void createFeatureDefinition(@RequestBody CreateFeatureDefinitionRequest request) throws IOException {
+    public void createFeatureDefinition(@RequestBody CreateFeatureDefinitionRequest request) {
         var response = projectService.createFeatureDefinition(request);
         sseEmitterManager.sendEvent("create-feature-definition", response);
     }
 
     @PutMapping("/definition")
-    public void feedbackFeatureDefinition(@RequestBody FeedbackRequest request) throws IOException {
+    public void feedbackFeatureDefinition(@RequestBody FeedbackRequest request) {
         var response = projectService.feedbackFeatureDefinition(request);
         sseEmitterManager.sendEvent("feedback-feature-definition", response);
     }
 
     @GetMapping("/specification")
-    public void createFeatureSpecification() throws IOException {
+    public void createFeatureSpecification() {
         var response = projectService.createFeatureSpecification();
         sseEmitterManager.sendEvent("create-feature-specification", response);
     }
 
     @PutMapping("/specification")
-    public void feedbackFeatureSpecification(@RequestBody FeedbackRequest request) throws IOException {
+    public void feedbackFeatureSpecification(@RequestBody FeedbackRequest request) {
         var response = projectService.feedbackFeatureSpecification(request);
         sseEmitterManager.sendEvent("feedback-feature-specification", response);
     }
