a3887a85cd45cb4b210079e854dba200c43c6e1c
yeonnnnjs <sungyeon52@gmail.com>
2025-05-12 17:40:43 +0900

    feat: Sprint 생성 테스트 API 추가 (CM-105)

--- src/main/java/com/starmix/checkmate/adapter/in/http/test/TestController.java
+++ src/main/java/com/starmix/checkmate/adapter/in/http/test/TestController.java
@@ -1,12 +1,15 @@
 package com.starmix.checkmate.adapter.in.http.test;
 
+import com.starmix.checkmate.adapter.in.http.test.request.CreateSprintTestRequest;
 import com.starmix.checkmate.adapter.in.sse.common.SseEmitterManager;
 import com.starmix.checkmate.adapter.in.sse.project.request.CreateFeatureDefinitionRequest;
 import com.starmix.checkmate.adapter.in.sse.project.request.FeedbackRequest;
 import com.starmix.checkmate.application.port.out.persistence.ProjectPersistencePort;
+import com.starmix.checkmate.application.port.out.persistence.SprintPersistencePort;
 import com.starmix.checkmate.application.port.out.persistence.UserPersistencePort;
 import com.starmix.checkmate.application.service.ProjectTestService;
 import com.starmix.checkmate.domain.project.Project;
+import com.starmix.checkmate.domain.sprint.Sprint;
 import com.starmix.checkmate.domain.user.User;
 import com.starmix.checkmate.global.exception.CustomException;
 import com.starmix.checkmate.infrastructure.security.JwtUtil;
@@ -29,6 +32,7 @@ public class TestController {
 
     private final ProjectTestService projectService;
     private final SseEmitterManager sseEmitterManager;
+    private final SprintPersistencePort sprintPersistencePort;
 
     @PostMapping("/definition")
     public void createFeatureDefinition(@RequestBody CreateFeatureDefinitionRequest request) {
@@ -74,5 +78,16 @@ public class TestController {
         members.forEach(userPersistencePort::save);
         projectPersistencePort.save(project);
         return ResponseEntity.ok().body(project);
+    }
+
+    @Transactional
+    @PostMapping("/sprint/create")
+    public ResponseEntity<Sprint> createSprintTest (
+            @RequestBody CreateSprintTestRequest request
+    ) {
+        Integer sequence = sprintPersistencePort.getNextSequence();
+        Sprint sprint = request.toDomain(sequence);
+        sprintPersistencePort.save(sprint);
+        return ResponseEntity.ok().body(sprint);
     }
 }
